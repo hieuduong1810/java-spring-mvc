870864e24cb2adc3bb0d169805964559685a8f8e
Duong Hieu <hieuduong181005@gmail.com>
2025-05-15 21:08:02 +0700

    video 94

--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -1,10 +1,51 @@
 package vn.hoidanit.laptopshop.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import vn.hoidanit.laptopshop.domain.Role;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.RoleRepository;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
     public String handleHello() {
         return "Hello from service";
+    }
+
+    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public List<User> getAllUserByEmail(String email) {
+        return this.userRepository.findOneByEmail(email);
+    }
+
+    public User handleSaveUser(User user) {
+        User hieuduong = this.userRepository.save(user);
+        System.out.println(hieuduong);
+        return hieuduong;
+    }
+
+    public User getUserById(long id) {
+        return this.userRepository.findById(id);
+    }
+
+    public void deleteAUser(long id) {
+        this.userRepository.deleteById(id);
+    }
+
+    public Role getRoleByName(String name) {
+        return this.roleRepository.findByName(name);
     }
 }
